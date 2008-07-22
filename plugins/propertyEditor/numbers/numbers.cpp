89c43c13868b082fdcfa5808e8b895c6e8c44c8b
taipanromania <taipanromania@localhost>
2008-07-22 14:49:08 +0000

    set step for doubles

--- plugins/propertyEditor/numbers/numbers.cpp
+++ plugins/propertyEditor/numbers/numbers.cpp
@@ -39,6 +39,7 @@ QWidget* Numbers::createEditor(QWidget * parent, const QModelIndex & index)
 		QDoubleSpinBox *dsb = new QDoubleSpinBox(parent);
 		dsb->setMaximum(1e10);
 		dsb->setMinimum(-1e10);
+		dsb->setSingleStep(0.1);
 		connect(dsb, SIGNAL(valueChanged(double)), this, SLOT(setValue(double)));
 		return dsb;
 	}
