dbfff3b18e530b4ad203ffc4b966488fa32c86ae
nacuflorina <nacuflorina@localhost>
2009-04-17 12:51:50 +0000

    adaugat pieChart

--- plugins/report/items/charts/piechart/piechart.h
+++ plugins/report/items/charts/piechart/piechart.h
@@ -1,7 +1,7 @@
 /***************************************************************************
  *   This file is part of the eXaro project                                *
- *   Copyright (C) 2008 by BogDan Vatra                                    *
- *   bog_dan_ro@yahoo.com                                                  *
+ *   Copyright (C) 2009 Florina Nacu                                       *
+ *   nacuflorina@gmail.com                                                 *
  **                   GNU General Public License Usage                    **
  *                                                                         *
  *   This library is free software: you can redistribute it and/or modify  *
@@ -27,31 +27,24 @@
  *   GNU General Public License for more details.                          *
  ****************************************************************************/
 
-#ifndef PIECHART_H
-#define PIECHART_H
+#ifndef BARCHART_H
+#define BARCHART_H
 
-#include "abstractchartitem.h"
+#include "chartinterface.h"
 
-class QAbstractItemModel;
-
-class PieChart : public AbstractChartItem
+class PieChart : public Report::ChartInterface
 {
 	Q_OBJECT
-	Q_INTERFACES(Report::ItemInterface);
-	Q_PROPERTY(QStringList dataList READ dataList WRITE setDataList)
-//	Q_PROPERTY(QFile dataFile READ dataFile WRITE setDataFIle)
-	Q_PROPERTY(int startAngle READ startAngle() WRITE setStartAngle)
+	Q_INTERFACES(Report::ChartInterface);
+
+//	Q_PROPERTY(bool show3d READ show3d WRITE setShow3d)
+	Q_PROPERTY(bool showLabels READ showLabels WRITE setShowLabels)
+	Q_PROPERTY(bool showGrid READ showGrid WRITE setShowGrid)
+	Q_PROPERTY(bool drawBorder READ drawBorder WRITE setDrawBorder)
+	Q_PROPERTY(int barsIdentation READ barsIdentation WRITE setBarsIdentation)
+	Q_PROPERTY(qreal toColorFactor READ toColorFactor WRITE setToColorFactor)
 
 public:
-/*
-    enum LegendFlag {
-        NoFlags=0, Shadow=1, SeriesClickable=2
-    };
-     Q_DECLARE_FLAGS(LegendFlags, LegendFlag)
-    ChartLegend(LegendFlags f=NoFlags){ m_flags = f;}
-    LegendFlags flags() const{ return m_flags;}
-    void setFlags(LegendFlags f){ m_flags = f; }
-*/
 	PieChart(QGraphicsItem* parent = 0, QObject* parentObject = 0);
 
 	QRectF boundingRect() const;
@@ -61,54 +54,33 @@ public:
 	QString toolBoxText();
 	QString toolBoxGroup();
 
-	bool canContain(QObject * object);
-
-
-	int legendWidth() const;
- 	void setLegendWidth(const int& theValue);
-
-	void setStartAngle(int deg );
-	int startAngle();
-
-	void openFile(const QString &path = QString());
-	void calculateTotalValue();
-
-	QStringList dataList();
-	void setDataList(QStringList chartData);
-
-//	QFile dataFile();
-//	void setDataFile(QFile file);
-
-/*
-	qreal xRadius();
-	void setXRadius(qreal radius);
-
-	qreal yRadius();
-	void setYRadius(qreal radius);
-*/
 	QObject * createInstance(QGraphicsItem* parent = 0, QObject* parentObject = 0);
+
+	int barsIdentation();
+	void setBarsIdentation(int barsIdentation);	
+	int distanta(QPointF x,QPointF y);
+//	bool show3d();
+//	void setShow3d(bool show3d);
+
+	bool drawBorder();
+	void setDrawBorder(bool drawBorder);
+
+	bool showGrid();
+	void setShowGrid(bool showGrid);
+
+	bool showLabels();
+	void setShowLabels(bool showLabels);
+
+	qreal toColorFactor();
+	void setToColorFactor(qreal toColorFactor);
+
 private:
-	void drawChart(QPainter * painter, const QStyleOptionGraphicsItem * option);
-	void drawLegend(QPainter * painter, const QStyleOptionGraphicsItem * option);
-	
-	QSize chartTitleSize() const;
-	QSize xTitleSize() const;
-   QSize yTitleSize() const;
-	QSize rowNameSize() const;
-   QSize valueSize() const;
-
-	QFont m_legendFont;
-
-	int m_legendWidth;
-	int m_startAngle;
-	double m_totalValue;
-   int m_validItems;
-	QRect chartRect;
-
-	QAbstractItemModel* model;
-	QStringList m_chartData;
-
-	//qreal m_xRadius, m_yRadius;
+	int m_barsIdentation;
+//	bool m_show3d;
+	bool m_showLabels;
+	qreal m_toColorFactor;
+	bool m_drawBorder;
+	bool m_showGrid;
 };
 
 #endif
