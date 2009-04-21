9e5a7908e3c2983c9a46e8830f67a72dda5a2464
foxysoft <foxysoft@localhost>
2009-04-21 20:25:13 +0000

    CSV Dataset plugin done (?)

--- plugins/report/dataset/csv/model.h
+++ plugins/report/dataset/csv/model.h
@@ -49,8 +49,8 @@ public:
 
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
     Qt::ItemFlags flags(const QModelIndex &index) const;
-    QVariant headerData(int section, Qt::Orientation orientation,
-                        int role = Qt::DisplayRole) const;
+//    QVariant headerData(int section, Qt::Orientation orientation,
+//                        int role = Qt::DisplayRole) const;
 //    QModelIndex index(int row, int column,
 //                      const QModelIndex &parent = QModelIndex()) const;
 //    QModelIndex parent(const QModelIndex &child) const;
