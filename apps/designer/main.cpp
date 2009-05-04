ab3238b7baf52cfad47a8e5ccd897803bae6e7aa
taipanromania <taipanromania@localhost>
2009-05-04 18:08:12 +0000

    Ladies and gentlemen I'm proud to present you the new bullet-proof eXaro. From now on no one and nothing will crash eXaro. All runtime errors (/ by 0, segfaults) are transformed in exceptions.

--- apps/designer/main.cpp
+++ apps/designer/main.cpp
@@ -13,8 +13,6 @@
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
  ***************************************************************************/
-
-
 #include <QTranslator>
 #include <QDir>
 
@@ -23,8 +21,70 @@
 #include "exaroapplication.h"
 #include "mainwindow.h"
 
+#if defined __GNUC__ && __linux
+
+#ifdef __i386__
+#include "i386-signal.h"
+#endif /*__i386__*/
+
+#ifdef __x86_64__
+#include "x86_64-signal.h"
+#endif /*__x86_64__*/
+
+#endif /*defined __GNUC__ && __linux*/
+
+
+#if defined (HANDLE_SEGV) || defined(HANDLE_FPE)
+
+/* Unblock a signal.  Unless we do this, the signal may only be sent
+   once.  */
+static void
+unblock_signal (int signum __attribute__ ((__unused__)))
+{
+#ifdef _POSIX_VERSION
+	sigset_t sigs;
+	sigemptyset (&sigs);
+	sigaddset (&sigs, signum);
+	sigprocmask (SIG_UNBLOCK, &sigs, NULL);
+#endif
+}
+#endif
+
+
+#ifdef HANDLE_SEGV
+SIGNAL_HANDLER (catch_segv)
+{
+	unblock_signal (SIGSEGV);
+	MAKE_THROW_FRAME (nullp);
+	QString nullp="Null pointer";
+	throw nullp;
+}
+#endif
+
+#ifdef HANDLE_FPE
+SIGNAL_HANDLER (catch_fpe)
+{
+	unblock_signal (SIGFPE);
+#ifdef HANDLE_DIVIDE_OVERFLOW
+	HANDLE_DIVIDE_OVERFLOW;
+#else
+	MAKE_THROW_FRAME (arithexception);
+#endif
+	QString arithexception="/ by zero";
+	throw arithexception;
+}
+#endif
+
 int main(int argc, char **argv)
 {
+	#ifdef HANDLE_SEGV
+	INIT_SEGV;
+	#endif
+
+	#ifdef HANDLE_FPE
+	INIT_FPE;
+	#endif
+
 	QApplication::setOrganizationName("licentia");
 	QApplication::setOrganizationDomain("licentia.eu");
 	QApplication::setApplicationName("eXaro");
