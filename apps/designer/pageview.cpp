2d5f6c69975c57ab918fbdedcaab43e833e50d8a
foxysoft <foxysoft@localhost>
2009-06-08 18:04:58 +0000

    Ruler implemented for designer (Thanks Koffice team for part of code)

--- apps/designer/pageview.cpp
+++ apps/designer/pageview.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Mikhalov Alexander                              *
+ *   alexmi3@rambler.ru                                                    *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
 #include "pageview.h"
 #include "QGridLayout"
 #include <QtGui/QGraphicsScene>
@@ -7,11 +23,21 @@
 #include "qruler.h"
 #include "qunit.h"
 
+/*
+GraphicsView::GraphicsView ( QGraphicsScene * scene, QWidget * parent)
+	:QGraphicsView (scene, parent)
+{
+
+}
+*/
+
 PageView::PageView(QGraphicsScene *  scene, QWidget * parent, Qt::WindowFlags f )
 	: QWidget(parent, f ),
 	m_scene(scene)
 {
-    m_view = new QGraphicsView ( m_scene, this );
+    m_view = new GraphicsView ( m_scene, this );
+//    m_unit = new QUnit();
+//    m_unit->
 
     QGridLayout *gridLayout = new QGridLayout(this);
     gridLayout->setMargin(0);
@@ -19,13 +45,13 @@ PageView::PageView(QGraphicsScene *  scene, QWidget * parent, Qt::WindowFlags f 
 
     m_horizontalRuler = new QRuler(this, Qt::Horizontal);
     m_horizontalRuler->setShowMousePosition(true);
-    m_horizontalRuler->setUnit(QUnit());
+    m_horizontalRuler->setUnit(QUnit(QUnit::Millimeter));
 //    m_horizontalRuler->setRulerLength(m_view->width());
 //    m_horizontalRuler->setActiveRange(200,300/*gw->width()*/);
 
     m_verticalRuler = new QRuler(this, Qt::Vertical);
     m_verticalRuler->setShowMousePosition(true);
-    m_verticalRuler->setUnit(QUnit());
+    m_verticalRuler->setUnit(QUnit(QUnit::Millimeter));
 //    m_verticalRuler->setRulerLength( m_view->height() );
 //    m_verticalRuler->setActiveRange(0, m_view->height() );
 
@@ -39,8 +65,8 @@ PageView::PageView(QGraphicsScene *  scene, QWidget * parent, Qt::WindowFlags f 
     //connect ( m_view->horizontalScrollBar() , SIGNAL (valueChanged(int)), m_horizontalRuler, SLOT(setOffset(int) ) );
     connect ( m_view->horizontalScrollBar() , SIGNAL (valueChanged(int)), this, SLOT(doHorizontalScroll(int) ) );
     connect ( m_view->verticalScrollBar() , SIGNAL (valueChanged(int)), this, SLOT(doVerticalScroll(int) ) );
+    connect ( m_view, SIGNAL ( mousePositionChanged(QPoint) ), this, SLOT ( mousePositionChanged(QPoint) ) );
 
-    this->setMouseTracking(true);
 }
 
 void PageView::resizeEvent ( QResizeEvent * event )
@@ -50,12 +76,20 @@ void PageView::resizeEvent ( QResizeEvent * event )
     m_verticalRuler->setRulerLength( p.y() );
 }
 
+void PageView::mousePositionChanged(QPoint pos)
+{
+    m_horizontalRuler->updateMouseCoordinate(m_view->horizontalScrollBar()->value() + pos.x());
+    m_verticalRuler->updateMouseCoordinate(m_view->verticalScrollBar()->value() + pos.y());
+}
+
+/*
 void PageView::mouseMoveEvent ( QMouseEvent * e )
 {
     qDebug("mouse ");
     m_horizontalRuler->updateMouseCoordinate(e->x());
     m_verticalRuler->updateMouseCoordinate(e->y());
 }
+*/
 
 /*
 void PageView::scrollContentsBy ( int dx, int dy )
@@ -82,5 +116,5 @@ QGraphicsScene * PageView::scene() const
 
 QGraphicsView * PageView::view() const
 {
-    return m_view;
+    return (QGraphicsView *)m_view;
 }
--- apps/designer/pageview.h
+++ apps/designer/pageview.h
@@ -1,12 +1,31 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Mikhalov Alexander                              *
+ *   alexmi3@rambler.ru                                                    *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
 #ifndef PAGEVIEW_H
 #define PAGEVIEW_H
 
 #include <QWidget>
+#include <QGraphicsView>
+#include <QMouseEvent>
 
 class QGraphicsScene;
-class QGraphicsView;
+class GraphicsView;
 class QRuler;
 class QUnit;
+
 
 class PageView : public QWidget
 {
@@ -20,18 +39,34 @@ public:
 protected:
     virtual void resizeEvent ( QResizeEvent * event );
 //    virtual void scrollContentsBy ( int dx, int dy );
-    virtual void mouseMoveEvent ( QMouseEvent * e );
+//    virtual void mouseMoveEvent ( QMouseEvent * e );
 
 private slots:
     void doHorizontalScroll(int value);
     void doVerticalScroll(int value);
+    void mousePositionChanged(QPoint pos);
 
 private:
     QGraphicsScene * m_scene;
-    QGraphicsView * m_view;
+    GraphicsView * m_view;
     QRuler * m_horizontalRuler;
     QRuler * m_verticalRuler;
     QUnit * m_unit;
 };
+
+class GraphicsView: public QGraphicsView
+{
+    Q_OBJECT
+public:
+    GraphicsView ( QGraphicsScene * scene, QWidget * parent = 0 ): QGraphicsView(scene, parent) {};
+signals:
+    void mousePositionChanged (QPoint pos);
+protected:
+    void mouseMoveEvent ( QMouseEvent * e )
+    {
+	emit mousePositionChanged ( e->pos () );
+    }
+};
+
 
 #endif // PAGEVIEW_H
