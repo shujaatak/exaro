3074a329e4ec0888a1134c47c563a1a71a70e0ae
Denis Kormalev <kormalev.denis@gmail.com>
2010-07-19 16:01:10 +0400

    Small fix for mobile platforms.

--- libs/report/previewwidget.cpp
+++ libs/report/previewwidget.cpp
@@ -14,11 +14,11 @@
  **                  GNU Lesser General Public License                    **
  *                                                                         *
  *   This library is free software: you can redistribute it and/or modify  *
- *   it under the terms of the GNU Lesser General Public License as        * 
+ *   it under the terms of the GNU Lesser General Public License as        *
  *   published by the Free Software Foundation, either version 3 of the    *
  *   License, or (at your option) any later version.                       *
  *   You should have received a copy of the GNU Lesser General Public      *
- *   License along with this library.                                      * 
+ *   License along with this library.                                      *
  *   If not, see <http://www.gnu.org/licenses/>.                           *
  *                                                                         *
  *   This library is distributed in the hope that it will be useful,       *
@@ -29,7 +29,10 @@
 
 #include <QSettings>
 #include <QDesktopWidget>
+
+#if !defined(Q_OS_SYMBIAN)&&!defined(Q_WS_SIMULATOR)&&!defined(Q_WS_MAEMO_5)&&!defined(ANDROID)
 #include <QGLWidget>
+#endif
 #include "previewwidget.h"
 
 namespace Report
@@ -38,12 +41,12 @@ namespace Report
 #define screen_widthMM (((double)QDesktopWidget().screen()->width() /(double)QDesktopWidget().screen()->physicalDpiX() )*25.4)
 
 PreviewWidget::PreviewWidget(QWidget *parent)
-		: QGraphicsView(parent)
+        : QGraphicsView(parent)
 {
-	m_currentZoom=(double)QDesktopWidget().screen()->width()/(screen_widthMM*10);
-	m_zoomMin=m_currentZoom/4;
-	m_zoomMax=m_currentZoom*4;
-	m_zoomStep=m_currentZoom/4;
+    m_currentZoom=(double)QDesktopWidget().screen()->width()/(screen_widthMM*10);
+    m_zoomMin=m_currentZoom/4;
+    m_zoomMax=m_currentZoom*4;
+    m_zoomStep=m_currentZoom/4;
 
 #if defined(ANDROID)||defined(Q_WS_MAEMO_5)||defined(Q_WS_S60)||defined(Q_WS_SIMULATOR)
     setDragMode(ScrollHandDrag);
@@ -61,54 +64,60 @@ PreviewWidget::~PreviewWidget()
 
 void PreviewWidget::toggleOpenGl(bool useOpenGL)
 {
-	QSettings s;
-	s.setValue("eXaro/useOpenGL", useOpenGL);
-	setViewport(useOpenGL?(new QGLWidget):(new QWidget));
+    QSettings s;
+    s.setValue("eXaro/useOpenGL", useOpenGL);
+
+#if defined(Q_OS_SYMBIAN)||defined(Q_WS_SIMULATOR)||defined(Q_WS_MAEMO_5)||defined(ANDROID)
+    setViewport(new QWidget);
+#else
+    setViewport(useOpenGL?(new QGLWidget):(new QWidget));
+#endif
+
 }
 
 void PreviewWidget::zoomIn()
 {
-	m_currentZoom += m_zoomStep;
-	if (m_currentZoom > m_zoomMax)
-		m_currentZoom = m_zoomMax;
-	emit(zoomChanged((int)((m_currentZoom/((double)QDesktopWidget().screen()->width()/(screen_widthMM*10)))*100 + 0.5)));
+    m_currentZoom += m_zoomStep;
+    if (m_currentZoom > m_zoomMax)
+        m_currentZoom = m_zoomMax;
+    emit(zoomChanged((int)((m_currentZoom/((double)QDesktopWidget().screen()->width()/(screen_widthMM*10)))*100 + 0.5)));
 }
 
 void PreviewWidget::zoomOut()
 {
-	m_currentZoom -= m_zoomStep;
-	if (m_currentZoom < m_zoomMin)
-		m_currentZoom = m_zoomMin;
-	emit(zoomChanged((int)((m_currentZoom/((double)QDesktopWidget().screen()->width()/(screen_widthMM*10)))*100 + 0.5)));
+    m_currentZoom -= m_zoomStep;
+    if (m_currentZoom < m_zoomMin)
+        m_currentZoom = m_zoomMin;
+    emit(zoomChanged((int)((m_currentZoom/((double)QDesktopWidget().screen()->width()/(screen_widthMM*10)))*100 + 0.5)));
 }
 
 void PreviewWidget::zoomTo(int zoom)
 {
-	qreal z=((double)zoom/100)*((double)QDesktopWidget().screen()->width()/(screen_widthMM*10));
-	if (z > m_zoomMax)
-		z = m_zoomMax;
+    qreal z=((double)zoom/100)*((double)QDesktopWidget().screen()->width()/(screen_widthMM*10));
+    if (z > m_zoomMax)
+        z = m_zoomMax;
 
-	if (z < m_zoomMin)
-		z = m_zoomMin;
+    if (z < m_zoomMin)
+        z = m_zoomMin;
 
-	resetTransform();
-	scale(z, z);
-	m_currentZoom = z;
+    resetTransform();
+    scale(z, z);
+    m_currentZoom = z;
 }
 
 void PreviewWidget::setZoomStep(qreal step)
 {
-	m_zoomStep = step;
+    m_zoomStep = step;
 }
 
 void PreviewWidget::setZoomMax(qreal max)
 {
-	m_zoomMax = max;
+    m_zoomMax = max;
 }
 
 void PreviewWidget::setZoomMin(qreal min)
 {
-	m_zoomMin = min;
+    m_zoomMin = min;
 }
 
 }
